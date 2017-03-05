1fcac3f38bd3111a92564713260607cfc6cf14ea
drscotthawley <scott@scotthawley.com>
2017-03-05 14:44:04 -0600

    utils to split & join audio files; are inverses

---
+++ audio_utils/concat_audio.py
@@ -0,0 +1,56 @@
+'''
+concat_audio.py
+Author: Scott Hawley
+
+Joins a bunch of audio files into one big file.
+Filename of output is whatever the last filename is + "_L"
+
+Works on mono, stereo,...arbitrary numbers of channels
+
+'''
+from __future__ import print_function
+
+import numpy as np
+import librosa
+import os
+
+
+def main(args):
+	count = 0
+	for infile in args.file:
+		count += 1
+		if os.path.isfile(infile):
+			print("Input file: ",infile,"... ",end="",sep="")
+			signal, sr = librosa.load(infile, sr=None, mono=False)   # don't assume sr or mono
+			channels = signal.ndim    
+			axis = channels - 1
+			if (1 == channels):
+				print("this is a mono file.  signal.shape = ",signal.shape)
+			else:
+				print("this is a multi-channel file: signal.shape = ",signal.shape)
+
+
+			if (1 == count):
+				long_clip = signal
+			else:
+				long_clip = np.concatenate((long_clip,signal),axis=axis)
+
+		else:
+			print(" *** File",infile,"does not exist.  Skipping.")
+
+	filename_no_ext = os.path.splitext(infile)[0]
+	ext = '.wav'  # os.path.splitext(infile)[1]
+	outfile = filename_no_ext+"_L"+ext
+	print("Saving file",outfile)
+	librosa.output.write_wav(outfile,long_clip,sr)
+	return
+
+
+
+if __name__ == "__main__":
+	import argparse
+	parser = argparse.ArgumentParser(description="split_audio: splits file(s) into multiple clips")
+	parser.add_argument('file', help="file(s) to split", nargs='+')   
+	args = parser.parse_args()
+	main(args)
+

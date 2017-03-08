608335c6bf8884aad358abdb0c107839bce82d8a
Scott Hawley <scott.hawley@belmont.edu>
2017-03-07 22:11:43 -0600

    cleaned up single-class prediction

--- predict_class.py
+++ predict_class.py
@@ -11,33 +11,12 @@ from panotti.models import *
 from panotti.datautils import *
 
 
+def predict_class(signal, sr, class_names, model=None):
+    X = make_melgram(signal,sr)
+    if model is None:
+        model = load_model(X, class_names, no_cp_fatal=True)
+    return model.predict_proba(X,batch_size=1)[0], model
 
-def main(args):
-    np.random.seed(1)
-
-    file_count = 0
-    for infile in args.file:
-        if os.path.isfile(infile):
-            file_count += 1
-            print("Operating on file",infile,"...")
-
-            signal, sr = librosa.load(infile, mono=False, sr=44100)   # librosa naturally makes mono from stereo btw
-            X = make_melgram(signal,sr)
-
-            if (1==file_count):
-                class_names = get_class_names(path="Samples/")
-                nb_classes = len(class_names)
-                model = load_model(X, class_names, no_cp_fatal=True)
-
-            y_proba = model.predict_proba(X,batch_size=1)[0]
-            print("    ",infile,": ",end="")
-            for i in range(nb_classes):
-                print( class_names[i],": ",y_proba[i],", ",end="",sep="")
-            print("--> ANSWER:", class_names[ np.argmax(y_proba)])
-        else:
-            print(" *** File",infile,"does not exist.  Skipping.")
- 
-    return
 
 
 if __name__ == '__main__':
@@ -46,4 +25,25 @@ if __name__ == '__main__':
 #   parser.add_argument("n_elev", help="number of discrete poitions of elevation",type=int)
     parser.add_argument('file', help="file(s) to classify", nargs='+')   
     args = parser.parse_args()
-    main(args)
+    model = None
+
+    np.random.seed(1)
+    class_names = get_class_names(path="Preproc/Test/")
+    nb_classes = len(class_names)
+
+    file_count = 0
+    for infile in args.file:
+        if os.path.isfile(infile):
+            file_count += 1
+            print("Operating on file",infile,"...")
+
+            signal, sr = librosa.load(infile, mono=False, sr=44100)   # librosa naturally makes mono from stereo btw
+            y_proba, model = predict_class(signal, sr, class_names, model)
+
+            print("    ",infile,": ",end="")
+            for i in range(nb_classes):
+                print( class_names[i],": ",y_proba[i],", ",end="",sep="")
+            print("--> ANSWER:", class_names[ np.argmax(y_proba)])
+        else:
+            print(" *** File",infile,"does not exist.  Skipping.")
+ 

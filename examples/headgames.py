9ea5b99fdbbabb32fb8f575c76a1ec241dbba59f
Scott Hawley <scott.hawley@belmont.edu>
2017-03-08 23:39:14 -0600

    got headgame working for reading from Samples dir

--- examples/headgames.py
+++ examples/headgames.py
@@ -3,18 +3,51 @@
 HeadGames -  GUI for demo-ing binaural source localization
 Author: Scott Hawley
 
-Still under construction
-'''
+"But if you want to win, you gotta learn how to play"--Foreigner, "Head Games"
 
+Still under construction
+Requirements:
+    pygame
+    librosa
+'''
+from __future__ import print_function
 import pygame
 import math
-#import librosa
+import librosa
+import os
+import sys
+sys.path.insert(0, '..')
+from panotti.models import *  
+from panotti.datautils import *
+#from predict_class import predict_one
+import glob
+import random
+import re
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+
+
+def predict_one(signal, sr, class_names, model=None, weights_file="weights.hdf5"):
+    X = make_layered_melgram(signal,sr)
+    if model is None:
+        model = load_model(X, class_names, no_cp_fatal=True, weights_file=weights_file)
+    return model.predict_proba(X,batch_size=1,verbose=0)[0], model
+
+
+def get_wav_file_list(path="binaural/Samples/",shuffle=True):
+    file_list = glob.glob(path+"*/*.wav")
+    if shuffle:
+        random.shuffle(file_list)
+    return file_list
+
+def parse_class_string(filename):
+    str = re.search('class.*\/',filename).group(0)[0:-1]
+    str2 = re.search('0a.*\.',str).group(0)[2:-1]
+    return float(str2)
 
 def draw_head(screen,origin,screensize):
     color=BLACK
@@ -24,13 +57,11 @@ def draw_head(screen,origin,screensize):
     head = pygame.draw.ellipse(screen, color, headbox)
     noserad = int(scale/15)
     nose = pygame.draw.circle(screen, color, (cx,int(headbox[1]+noserad/4)), noserad  )
-
     earw = scale/10
     earh = scale/4
     hw = headsize[0]
     earbox_l = (cx-hw/2-earw/2, cy-earh/2, earw, earh)
     earbox_r = (cx+hw/2-earw/2, earbox_l[1], earbox_l[2], earbox_l[3])
-
     ear_l = pygame.draw.ellipse(screen, color, earbox_l )
     ear_r = pygame.draw.ellipse(screen, color, earbox_r )
     return head
@@ -52,10 +83,9 @@ def draw_bounds(screen,origin,screensize,n_az):
         pygame.draw.line(screen, color, startpos, endpos)
     return
 
-def draw_guess(screen,origin,screensize,n_az,guess_az):
+def draw_pie(screen,origin,screensize,n_az,guess_az, color=GREEN):
     if guess_az is None:
         return
-    color = GREEN
     cx, cy, r  = origin[0],origin[1], int(screensize[1]*.5)-2
 
     guess_az_rad = guess_az*math.pi/180
@@ -77,9 +107,24 @@ def draw_guess(screen,origin,screensize,n_az,guess_az):
     return
 
 
+
+################ MAIN CODE #############
 def do_pygame(n_az=12):
     # Define some colors
      
+    # make a list of valid angles
+    angles = []
+    deg_sweep = 360/n_az
+    for n in range(n_az):
+        angles.append(-180+n*deg_sweep)
+    #print("angles = ",angles)
+    file_list=get_wav_file_list()
+    print("len(file_list) = ",len(file_list))
+
+    class_names=get_class_names(path="binaural/Samples/", sort=True)
+    model = None
+
+
     pygame.init()
      
     # Set the width and height of the screen [width, height]
@@ -93,7 +138,8 @@ def do_pygame(n_az=12):
      
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
-    guess_az = -90.0
+    guess_az = 0.0
+    true_az = 0.0
     deg_inc = 360/n_az
     # -------- Main Program Loop -----------
     while not done:
@@ -103,7 +149,20 @@ def do_pygame(n_az=12):
                 done = True
      
         # --- Game logic should go here
-     
+            if event.type == pygame.MOUSEBUTTONUP:
+                guess_az = guess_az + deg_inc
+
+                # load new file
+                infile = file_list[random.randint(0,len(file_list)-1)]
+                print("infile = ",infile)
+                signal, sr = librosa.load(infile, mono=False, sr=44100)   # librosa naturally makes mono from stereo btw
+                y_proba, model = predict_one(signal, sr, class_names, model=model, weights_file="binaural/weights.hdf5")
+                guess_az = angles[ np.argmax(y_proba)]
+                # get true az from filename
+                true_az = parse_class_string(infile)
+                print("guess_az = ",guess_az,"true_az = ",true_az)
+
+
         # --- Screen-clearing code goes here
      
         # Here, we clear the screen to white. Don't put other drawing commands
@@ -115,8 +174,8 @@ def do_pygame(n_az=12):
      
         # --- Drawing code should go here
         origin = (int(screensize[0]/2),int(screensize[1]/2))
-        guess_az = guess_az + deg_inc
-        draw_guess(screen,origin,screensize,n_az,guess_az)
+        draw_pie(screen,origin,screensize,n_az,true_az,color=RED) # show true
+        draw_pie(screen,origin,screensize,n_az,guess_az) # show guess
         draw_bounds(screen,origin,screensize,n_az)
 
         draw_head(screen,origin,screensize)
@@ -133,4 +192,5 @@ def do_pygame(n_az=12):
 
 if __name__ == "__main__":
     print("headgames.py - Still under construction, for now it just shows an animation")
+    #TODO: First check for existence of files we need: binaural/, binaural/Samples, binaural/weights.hdf5
     do_pygame(n_az=12)

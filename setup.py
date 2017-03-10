1a6d73c512fb6b4d7bf9f771d54b20153822a1d9
drscotthawley <scott@scotthawley.com>
2017-03-10 01:32:54 -0600

    making it packagy

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 from setuptools import find_packages
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 
 setup(name='Panotti',
       version='1.0.0',
       description='Multi-Channel Audio Classifier',
+      long_description=read_md('README.md'),
       author='Scott Hawley',
       author_email='scott.hawley@belmont.edu',
       url='https://github.com/drscotthawley/panotti',
@@ -14,4 +22,5 @@ setup(name='Panotti',
       extras_require={
           'headgames': ['pygame'],
       },
-      packages=find_packages()) 
+      packages=find_packages()
+) 

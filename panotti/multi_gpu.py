95e7a0873ddd08ca595c499361ecdcecfc0c98aa
Scott Hawley <scott.hawley@belmont.edu>
2019-08-31 13:19:38 -0500

    removed unused code

--- panotti/multi_gpu.py
+++ panotti/multi_gpu.py
@@ -55,11 +55,12 @@ class MultiGPUModelCheckpoint(Callback):
             saved (`model.save_weights(filepath)`), else the full model
             is saved (`model.save(filepath)`).
         period: Interval (number of epochs) between checkpoints.
+        ## Not implmented pb: Boolan: Also save weights to Protobuf file for compatibility with Tensorflow
     """
 
     def __init__(self, filepath, monitor='val_loss', verbose=0,
                  save_best_only=False, save_weights_only=False,
-                 mode='auto', period=1, serial_model=None, class_names=None):
+                 mode='auto', period=1, serial_model=None, class_names=None)# , pb=False):
         super(MultiGPUModelCheckpoint, self).__init__()
         self.monitor = monitor
         self.verbose = verbose
@@ -132,58 +133,3 @@ class MultiGPUModelCheckpoint(Callback):
                 else:
                     #self.serial_model.save(filepath, overwrite=True)
                     models.save_model_ext(self.serial_model, filepath, overwrite=True, class_names=self.class_names)
-
-
-'''
-Old code, before FChollet integrated into Keras 2.0.9:
-# from https://github.com/kuza55/keras-extras/blob/master/utils/multi_gpu.py
-# SHHawley updated to Keras 2, added make_serial & get_available_gpus, and gpu_count=-1 flag
-
-
-def make_parallel(model, gpu_count=-1):
-    if (gpu_count < 0):                 # gpu_count < 0 will mean  "use all available GPUs"
-        gpus =  get_available_gpus()
-        gpu_count = len(gpus)
-        print("\n make_parallel:",gpu_count,"GPUs detected.")
-    print(" make_parallel: Data parallel execution across",gpu_count,"GPUs")
-    def get_slice(data, idx, parts):
-        shape = tf.shape(data)
-        size = tf.concat([ shape[:1] // parts, shape[1:] ],axis=0)
-        stride = tf.concat([ shape[:1] // parts, shape[1:]*0 ],axis=0)
-        start = stride * idx
-        return tf.slice(data, start, size)
-
-    outputs_all = []
-    for i in range(len(model.outputs)):
-        outputs_all.append([])
-
-    #Place a copy of the model on each GPU, each getting a slice of the batch
-    for i in range(gpu_count):
-        with tf.device('/gpu:%d' % i):
-            with tf.name_scope('tower_%d' % i) as scope:
-
-                inputs = []
-                #Slice each input into a piece for processing on this GPU
-                for x in model.inputs:
-                    input_shape = tuple(x.get_shape().as_list())[1:]
-                    slice_n = Lambda(get_slice, output_shape=input_shape, arguments={'idx':i,'parts':gpu_count})(x)
-                    inputs.append(slice_n)
-
-                outputs = model(inputs)
-
-                if not isinstance(outputs, list):
-                    outputs = [outputs]
-
-                #Save all the outputs for merging back together later
-                for l in range(len(outputs)):
-                    outputs_all[l].append(outputs[l])
-
-    # merge outputs on CPU
-    with tf.device('/cpu:0'):
-        merged = []
-        for outputs in outputs_all:
-            #merged.append(merge(outputs, mode='concat', concat_axis=0))
-            merged.append(concatenate(outputs, axis=0))
-
-        return Model(inputs=model.inputs, outputs=merged)
-'''
